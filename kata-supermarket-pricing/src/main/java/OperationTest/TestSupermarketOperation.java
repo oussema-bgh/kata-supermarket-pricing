7103326fdae787af3eee67941c33e408fcfd8cdf
oussema-bgh <ben.ghorbel.med.oussema@gmail.com>
2022-09-28 11:59:57 +0100

    Update TestSupermarketOperation.java
    add new Test

--- kata-supermarket-pricing/src/main/java/OperationTest/TestSupermarketOperation.java
+++ kata-supermarket-pricing/src/main/java/OperationTest/TestSupermarketOperation.java
@@ -16,23 +16,31 @@ public class TestSupermarketOperation {
 	 private SupermarketOperation aSupermarket = new SupermarketOperation();
 	 private Operation aCustomer = new Operation();
 
+	 	@Test
+	 	public void item_in_Promotion_Check() {
+	        Item item = new Item("sweep", 50);
+	        aSupermarket.addReduction(item, 2, 10);
+			assertEquals(aSupermarket.itemPromotionCheck(item), true );
+	 	}
 
 	    @Test
 	    public void item_should_correctly_be_updated_when_reduction_set() {
 	        //given
+	    	
+	    	
 	        Item item = new Item("sweep", 50);
 
 	        //when
 	        aSupermarket.addReduction(item, 2, 10);
 			Optional<Integer> numberForReductionn = item.getReductionValueByNumber().entrySet().stream().map(Map.Entry::getKey)
 					  .findFirst();
-			Optional<Integer> reductionValuee = item.getReductionValueByNumber().entrySet().stream().map(Map.Entry::getKey)
+			Optional<Double> reductionValuee = item.getReductionValueByNumber().entrySet().stream().map(Map.Entry::getValue)
 					  .findFirst();
 	        //then
 			assertEquals(numberForReductionn.get(), 2,0.001);
-			assertEquals(reductionValuee.get(), 5,0.001);
+			assertEquals(reductionValuee.get(), 10,0.001);
 	    }
-
+	    
 	    @Test
 	    public void item_reduction_should_correctly_be_replaced_when_new_reduction_set() {
 	        //given
@@ -44,7 +52,7 @@ public class TestSupermarketOperation {
 
 			Optional<Integer> numberForReductionn = anItem.getReductionValueByNumber().entrySet().stream().map(Map.Entry::getKey)
 					  .findFirst();
-			Optional<Integer> reductionValuee = anItem.getReductionValueByNumber().entrySet().stream().map(Map.Entry::getKey)
+			Optional<Double> reductionValuee = anItem.getReductionValueByNumber().entrySet().stream().map(Map.Entry::getValue)
 					  .findFirst();
 	        //then
 			assertEquals(numberForReductionn.get(), 2,0.001);
@@ -68,24 +76,26 @@ public class TestSupermarketOperation {
 			Optional<Integer> reductionValuee = anItem.getReductionValueByNumber().entrySet().stream().map(Map.Entry::getKey)
 					  .findFirst();
 	        //then
-			assertEquals(numberForReductionn.get(), null );
-			assertEquals(reductionValuee.get(), null );
+			assertEquals(numberForReductionn.isPresent(), false );
+			assertEquals(reductionValuee.isPresent(), false );
 	    }
-
+ 
 	    @Test
 	    public void price_should_be_correct_when_calculating_bill() {
 	        //given
  	        Item aSweep = new Item(  "sweep", 50);
 	        Item aPotato = new Item( "potato", 5);
 	        Item aSoap = new Item( "soap", 10);
+	        
 	        aCustomer.addToCart(aSweep, 4);
 	        aCustomer.addToCart(aPotato, 10);
 	        aCustomer.addToCart(aSoap, 3);
-	        aSupermarket.addReduction(aSoap, 2, 10.2);
+	        
+	        aSupermarket.addReduction(aSoap, 3, 10);
 
 	        //when
 	        aSupermarket.calculateBill(aCustomer.returnCart());
 	        //then
-	        assertEquals(aSupermarket.calculateBill(aCustomer.returnCart()),(231),0.001);
-	    }
+	        assertEquals(aSupermarket.calculateBill(aCustomer.returnCart()),(260),0.001);
+	    } 
 }
