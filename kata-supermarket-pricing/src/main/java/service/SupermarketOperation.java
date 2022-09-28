6a52efe681a218e401048cffc4aad27defdd2dc0
oussema-bgh <ben.ghorbel.med.oussema@gmail.com>
2022-09-28 12:03:55 +0100

    remove trace
    remove trace

--- kata-supermarket-pricing/src/main/java/service/SupermarketOperation.java
+++ kata-supermarket-pricing/src/main/java/service/SupermarketOperation.java
@@ -29,9 +29,6 @@ public class SupermarketOperation implements ISupermarketOperation{
 	
     private Double processPackagePricing(LinkedHashMap<Item, Integer> inputMap, Double bill)
     {
-    	for (Item iterable_element : inputMap.keySet()) {
-    			System.out.println(iterable_element.toString()+"  "+ itemPromotionCheck(iterable_element));
-		}
         return inputMap.entrySet().stream()
                 .filter(item -> itemPromotionCheck(item.getKey()))
                 .map(item -> new PackagePricing().calculatePrice(item.getKey(), item.getValue()))
@@ -40,9 +37,6 @@ public class SupermarketOperation implements ISupermarketOperation{
 
     private Double processDefaultPricing(LinkedHashMap<Item, Integer> data, Double bill)
     {
-    	for (Item iterable_element : data.keySet()) {
-			System.out.println(iterable_element.toString()+"  "+ itemPromotionCheck(iterable_element));
-	}
         return data.entrySet().stream()
                 .filter(item -> !itemPromotionCheck(item.getKey()))
                 .map(item -> new DefaultPricing().calculatePrice(item.getKey(), item.getValue()))
@@ -51,7 +45,6 @@ public class SupermarketOperation implements ISupermarketOperation{
 
 	public void addReduction(Item item, int numberToBuy, double reduction) {
 		// TODO Auto-generated method stub
-        Item anItem = new Item("item", 14);
         LinkedHashMap<Integer, Double> data= new LinkedHashMap<Integer, Double>();
         data.put(numberToBuy,  reduction);
         item.setReductionValueByNumber(data);
