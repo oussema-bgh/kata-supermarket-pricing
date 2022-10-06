ca4e08ffced5d2936ff4e912e77cfa06c36756fb
oussema-bgh <ben.ghorbel.med.oussema@gmail.com>
2022-10-06 15:24:27 +0100

    rename interface SuperMarkerOperation

--- kata-supermarket-pricing/src/main/java/service/SupermarketOperation.java
+++ kata-supermarket-pricing/src/main/java/service/SupermarketOperation.java
@@ -1,60 +1,19 @@
 package service;
 
 import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
-import interfaces.ISupermarketOperation;
+import Exceptions.DataException;
 import model.Item;
+import model.Offer;
 
-public class SupermarketOperation implements ISupermarketOperation{
+public interface SupermarketOperation {
 
+	public Boolean itemPromotionCheck(Item item);
 
-	
-	public Boolean itemPromotionCheck(Item item) {
+	public double calculateBill(LinkedHashMap<Item, Float> items,LinkedHashMap<Item, Offer> lstOffer);
 
+	public void addReduction(Item item, Offer offer)  throws DataException;
 
-        return !item.getReductionValueByNumber().isEmpty();
-	}
-	
-	public double calculateBill(LinkedHashMap<Item, Integer> data) {
-		// TODO Auto-generated method stub
-		Double bill = (double) 0 ;
-
-	        bill = processDefaultPricing(data, bill);
-	        bill = processPackagePricing(data, bill);
-
-	        return bill;	}
-	
-    private Double processPackagePricing(LinkedHashMap<Item, Integer> inputMap, Double bill)
-    {
-        return inputMap.entrySet().stream()
-                .filter(item -> itemPromotionCheck(item.getKey()))
-                .map(item -> new PackagePricing().calculatePrice(item.getKey(), item.getValue()))
-                .reduce(bill, (f1, f2) -> f1 + f2);
-    }
-
-    private Double processDefaultPricing(LinkedHashMap<Item, Integer> data, Double bill)
-    {
-        return data.entrySet().stream()
-                .filter(item -> !itemPromotionCheck(item.getKey()))
-                .map(item -> new DefaultPricing().calculatePrice(item.getKey(), item.getValue()))
-                .reduce(bill, (f1, f2) -> f1 + f2);
-    }
-
-	public void addReduction(Item item, int numberToBuy, double reduction) {
-		// TODO Auto-generated method stub
-        LinkedHashMap<Integer, Double> data= new LinkedHashMap<Integer, Double>();
-        data.put(numberToBuy,  reduction);
-        item.setReductionValueByNumber(data);
-
-	}
-
-	public void removeReductions(Item item) {
-		// TODO Auto-generated method stub
-        item.setReductionValueByNumber( new LinkedHashMap<Integer, Double>());
-
-	}
-
+	public void removeReductions(Item item);
 }
