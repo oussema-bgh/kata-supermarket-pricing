799d569521b80e3fc79164fd499782182f9db113
oussema-bgh <ben.ghorbel.med.oussema@gmail.com>
2022-10-06 15:26:47 +0100

    add attribut byWeight in Item Class

--- kata-supermarket-pricing/src/main/java/model/Item.java
+++ kata-supermarket-pricing/src/main/java/model/Item.java
@@ -1,48 +1,45 @@
 package model;
 
-import java.util.LinkedHashMap;
-
 public class Item {
-	
-		private String name;
-	    private int unitaryPrice;
-	    
-	    private LinkedHashMap<Integer, Double> reductionValueByNumber  ;
-		
-	    
-	    
-	    public Item(String name, int unitaryPrice) {
-			super();
-			this.name = name;
-			this.unitaryPrice = unitaryPrice;
-			reductionValueByNumber = new LinkedHashMap<Integer, Double>();
-		}
-	    
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		
-		public int getUnitaryPrice() {
-			return unitaryPrice;
-		}
-		public void setUnitaryPrice(int unitaryPrice) {
-			this.unitaryPrice = unitaryPrice;
-		}
-		
-		public LinkedHashMap<Integer, Double> getReductionValueByNumber() {
-			return reductionValueByNumber;
-		}
-		public void setReductionValueByNumber(LinkedHashMap<Integer, Double> reductionValueByNumber) {
-			this.reductionValueByNumber = reductionValueByNumber;
-		}
-		
-		@Override
-		public String toString() {
-			return "Item [name=" + name + ", unitaryPrice=" + unitaryPrice + ", reductionValueByNumber="
-					+ reductionValueByNumber + " ]";
-		}
+
+	private String name;
+	private int unitaryPrice;
+	private boolean byWeight;
+
+	public Item(String name, int unitaryPrice, boolean byWeight) {
+		super();
+		this.name = name;
+		this.unitaryPrice = unitaryPrice;
+		this.byWeight = byWeight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getUnitaryPrice() {
+		return unitaryPrice;
+	}
+
+	public void setUnitaryPrice(int unitaryPrice) {
+		this.unitaryPrice = unitaryPrice;
+	}
+
+	public boolean isByWeight() {
+		return byWeight;
+	}
+
+	public void setByWeight(boolean byWeight) {
+		this.byWeight = byWeight;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [name=" + name + ", unitaryPrice=" + unitaryPrice + ", byWeight=" + byWeight + "]";
+	}
 
 }
