38400f97e20a7565004de5449dd3937e73246f88
oussema-bgh <ben.ghorbel.med.oussema@gmail.com>
2022-10-06 15:29:00 +0100

    Create Offer.java

---
+++ kata-supermarket-pricing/src/main/java/model/Offer.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Offer {
+	
+	private float qte;
+	private double price;
+
+	
+	
+	public Offer(float qte, double price) {
+		super();
+		this.qte = qte;
+		this.price = price;
+	}
+
+	public float getQte() {
+		return qte;
+	}
+
+	public void setQte(float qte) {
+		this.qte = qte;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Offer [qte=" + qte + ", price=" + price + "]";
+	}
+
+}
