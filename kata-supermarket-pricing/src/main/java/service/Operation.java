df64e93df3781c5fa991081c4266420289a58e9c
oussema-bgh <ben.ghorbel.med.oussema@gmail.com>
2022-10-06 15:23:46 +0100

    rename interface Operation

--- kata-supermarket-pricing/src/main/java/service/Operation.java
+++ kata-supermarket-pricing/src/main/java/service/Operation.java
@@ -2,50 +2,18 @@ package service;
 
 import java.util.LinkedHashMap;
 
-import interfaces.IOperation;
+import Exceptions.DataException;
+import Exceptions.ItemNotFoundException;
+import Exceptions.QuantityNotAvailableException;
 import model.Item;
 
-public class Operation implements IOperation {
+public interface Operation {
 
-	LinkedHashMap<Item, Integer> returnCart = new LinkedHashMap<Item, Integer> ();
-	
-	public LinkedHashMap<Item, Integer> returnCart() {
-		// TODO Auto-generated method stub
-		return returnCart;
-	}
+	LinkedHashMap<Item, Float> returnCart();
 
-	public void addToCart(Item item, Integer numberToBuy) {
-		// TODO Auto-generated method stub
-        if (returnCart.containsKey(item)) {
-        	returnCart.replace(item, numberToBuy +  returnCart.get(item));
-        }
-        else {
-		returnCart.put(item, numberToBuy);
-		}
-	}
+	void addToCart(Item item, float numberToBuy) throws DataException;
 
-	public void removeFromCart(Item item, Integer numberToReturn)throws RuntimeException {
-		// TODO Auto-generated method stub
-		 if (returnCart.containsKey(item))
-	        {
-	            if(returnCart.get(item)<numberToReturn) {
-	            	throw new RuntimeException("qte returned not existe in the Cart");
-	            }
-	            else if(returnCart.get(item)>numberToReturn){
-	            	returnCart.replace(item, returnCart.get(item) -  numberToReturn);
-	            }
-	            else if (returnCart.get(item)==numberToReturn){
-	            	returnCart.remove(item);
-	            }
-	        }
-		 else {
-         	throw new RuntimeException("Could not find the item");
-		 }
-	}
+	void removeFromCart(Item item, float numberToBuy) throws QuantityNotAvailableException, ItemNotFoundException, DataException;
 
-	public void emptyTheCart() {
-		// TODO Auto-generated method stub
-		returnCart.clear();
-	}
-
+	void emptyTheCart();
 }
